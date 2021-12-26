98c76d66cf5c1c2a2c525e55b862f73920764d87
rectanglehihat <80513184+rectanglehihat@users.noreply.github.com>
2021-12-26 23:29:08 +0900

    page CSS 적용

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,27 +1,66 @@
 <template>
   <div>
-    <p v-for="item in fetchedAsk" :key="item.id">
-      <router-link :to="`item/${item.id}`">
-        {{ item.title }}
-      </router-link>
-      <!-- <a :href="item.url">
-        {{ item.title }}
-      </a> -->
-      <small>{{ item.time_ago }} by {{ item.user }}</small>
-    </p>
+    <ul class="list">
+      <li v-for="item in fetchedAsk" :key="item.id" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="ask-title">
+            <router-link :to="`item/${item.id}`">
+              {{ item.title }}
+            </router-link>
+          </p>
+          <small class="link-text">
+            {{ item.time_ago }} by
+            <!-- {{ item.user }} -->
+            <router-link :to="`/user/${item.user}`" class="link-text">
+              {{ item.user }}
+            </router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-  import { mapGetters } from 'vuex';
-  export default {
-    computed: {
-      ...mapGetters(['fetchedAsk']),
-    },
-    created() {
-      this.$store.dispatch('FETCH_ASKS');
-    },
-  };
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters(["fetchedAsk"]),
+  },
+  created() {
+    this.$store.dispatch("FETCH_ASKS");
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #41b883;
+}
+.ask-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,18 +1,57 @@
 <template>
   <div>
-    <p v-for="job in this.$store.state.jobs" :key="job.id">
-      <a :href="job.url">{{ job.title }}</a>
-      <small>{{ job.time_ago }}{{ job.domain }}</small>
-    </p>
+    <ul class="list">
+      <li v-for="job in this.$store.state.jobs" :key="job.id" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ job.points || 0 }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="jobs-title">
+            <a :href="job.url">{{ job.title }}</a>
+          </p>
+          <small class="link-text">
+            {{ job.time_ago }}
+            <a :href="job.url">{{ job.domain }}</a>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-  export default {
-    created() {
-      this.$store.dispatch('FETCH_JOBS');
-    },
-  };
+export default {
+  created() {
+    this.$store.dispatch("FETCH_JOBS");
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #41b883;
+}
+.jobs-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
