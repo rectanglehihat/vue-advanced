f362da2eaa3976a614554c5077d5ba71e3f48d94
rectanglehihat <rectanglehihat@gmail.com>
2021-12-28 13:20:48 +0900

    실습: 외부 라이브러리 모듈화 방법(Chart)

---
+++ chart-lib/src/components/LineChart.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <canvas id="lineChart" width="400" height="400"></canvas>
+  </div>
+</template>
+
+<script>
+  import Chart from 'chart.js/auto';
+
+  export default {
+    mounted() {
+      const labels = ['January', 'February', 'March', 'April', 'May', 'June'];
+
+      const data = {
+        labels: labels,
+        datasets: [
+          {
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+          },
+        ],
+      };
+
+      const config = {
+        type: 'line',
+        data: data,
+        options: {},
+      };
+      console.log(config);
+
+      const myChart = new Chart(document.getElementById('lineChart'), config);
+      console.log(myChart);
+    },
+  };
+</script>
+
+<style></style>
